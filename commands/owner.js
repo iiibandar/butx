2eb0556f3e97f47f045e602467a11406c0e80b04
iiibandar <157326612+iiibandar@users.noreply.github.com>
2024-04-09 07:12:58 +0300

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -34,7 +34,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`I`m BANDAR `)
+            .setDescription(`I m BANDAR `)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
